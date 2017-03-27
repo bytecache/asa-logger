2c7306db173f1c37df5d01c4c64abdd92fe4d51d
Codename <cburwell@gmail.com>
2017-03-27 19:47:14 -0400

    Addeded exception handling

--- asa_logger.py
+++ asa_logger.py
@@ -4,6 +4,7 @@ import getpass
 import netmiko
 import os
 import time
+import sys
 
 #Clear screen
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -27,35 +28,61 @@ asa_enable_password = getpass.getpass("Enter ASA enable password: ")
 asa_host_focus = input("Enter the host IP address to search for in the logs: ")
 
 #Connect to ASA
-asa_ssh_connection = netmiko.ConnectHandler (
-	device_type='cisco_asa',
-	ip=asa_ip,
-	username=asa_username,
-	password=asa_password,
-	secret=asa_enable_password
-)
+try:
+	asa_ssh_connection = netmiko.ConnectHandler (
+		device_type='cisco_asa',
+		ip=asa_ip,
+		username=asa_username,
+		password=asa_password,
+		secret=asa_enable_password
+	)
+except netmiko.ssh_exception.NetMikoAuthenticationException:
+	#Handle authentication failures
+	sys.exit("[*] Error: Unable to authenticate")
+except netmiko.ssh_exception.NetMikoTimeoutException:
+	#Handle timeout failures
+	sys.exit("[*] Connection Timeout!")
 
 #Enter enable mode
 asa_ssh_connection.enable()
 
 asa_logs = asa_ssh_connection.send_command('show logging asdm')
 
-#loop
-try:
-	while True:
-		#Get current logs and append them to the existing variable
-		asa_logs += asa_ssh_connection.send_command('show logging asdm')
+#Check if asa_host_focus is empty
+if asa_host_focus == "":
+	#Collect logs until Ctrl+D
+	try:
+		while True:
+			#Get current logs and append them to the existing variable
+			asa_logs += asa_ssh_connection.send_command('show logging asdm')
 
-		#Clear the screen
-		os.system('cls' if os.name == 'nt' else 'clear')
+			#Clear the screen
+			os.system('cls' if os.name == 'nt' else 'clear')
 
-		#Print the logs
-		print (asa_logs)
+			#Print the logs
+			print (asa_logs)
 
-		#Wait before repeating
-		time.sleep(5)
-except KeyboardInterrupt():
-	pass
+			#Wait before repeating
+			time.sleep(5)
+	except KeyboardInterrupt():
+		pass
+else:
+	#Collect logs until Ctrl+D
+	try:
+		while True:
+			#Get current logs and append them to the existing variable
+			asa_logs += asa_ssh_connection.send_command('show logging asdm' + asa_host_focus)
+
+			#Clear the screen
+			os.system('cls' if os.name == 'nt' else 'clear')
+
+			#Print the logs
+			print (asa_logs)
+
+			#Wait before repeating
+			time.sleep(5)
+	except KeyboardInterrupt():
+		pass
 
 #Disconnect SSH session
 asa_ssh_connection.disconnect()
