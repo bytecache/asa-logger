6c3091f18e7bc1bd7b521afe2bf36a9910ab63c5
Codename <cburwell@gmail.com>
2017-03-30 19:46:17 -0400

    Added provisions for saving logs to a file

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 Summary
 ===
 * This script logs into an ASA, grabs the ASDM logs, stores them in a variable and prints the contents of that variable. The grabbing the logs and printing them occurs every five seconds until the script is terminated.
-* Version: 1.0
+* Version: 1.2
 
-Dependancies
+Dependencies
 ====
 * Tested with Python 3.5 & Netmiko 1.2.8
+* See requirements.txt for a full list
 
 Setup & Run
 ====
 * Run ASA logger using 'python asa_logger.py' or './asa_logger.py'
 * The shebang statement may need to be updated
 * Terminating the script can be accomplished using Ctrl+c or cmd+c
+* After terminating, the user has the ability to save logs to a file
+* The log file will be placed in the same directory from which the script was run
--- asa_logger.py
+++ asa_logger.py
@@ -6,28 +6,24 @@ import os
 import time
 import sys
 
-#Clear screen
+# Clear screen
 os.system('cls' if os.name == 'nt' else 'clear')
 
-#print banner
-terminal_width = os.get_terminal_size()
-print ("=" * terminal_width.columns)
-print ("ASA Logging Utility".center(terminal_width.columns))
-print ("-" * terminal_width.columns)
-print ("Description: Connects to an ASA and collects the contents on the logs")
-print ("Author: Codename")
-print ("=" * terminal_width.columns)
+# Print banner
+print ("#" * 70)
+print ("[*] ASA LOGGING UTILITY")
+print ("#" * 70)
 
-#prompt for ASA IP, username & password
+# Prompt for ASA IP, username & password
 asa_ip = input("Enter ASA IP address: ")
 asa_username = input("Enter ASA username: ")
 asa_password = getpass.getpass("Enter ASA password: ")
 asa_enable_password = getpass.getpass("Enter ASA enable password: ")
 
-#Prompt for IP to focus on
+# Prompt for IP to focus on
 asa_host_focus = input("Enter the host IP address to search for in the logs: ")
 
-#Connect to ASA
+# Connect to ASA
 try:
 	asa_ssh_connection = netmiko.ConnectHandler (
 		device_type='cisco_asa',
@@ -37,52 +33,62 @@ try:
 		secret=asa_enable_password
 	)
 except netmiko.ssh_exception.NetMikoAuthenticationException:
-	#Handle authentication failures
+	# Handle authentication failures
 	sys.exit("[*] Error: Unable to authenticate")
 except netmiko.ssh_exception.NetMikoTimeoutException:
-	#Handle timeout failures
+	# Handle timeout failures
 	sys.exit("[*] Connection Timeout!")
 
-#Enter enable mode
+# Enter enable mode
 asa_ssh_connection.enable()
 
 asa_logs = asa_ssh_connection.send_command('show logging asdm')
 
-#Check if asa_host_focus is empty
+# Check if asa_host_focus is empty
 if asa_host_focus == "":
-	#Collect logs until Ctrl+D
-	try:
-		while True:
-			#Get current logs and append them to the existing variable
-			asa_logs += asa_ssh_connection.send_command('show logging asdm')
-
-			#Clear the screen
-			os.system('cls' if os.name == 'nt' else 'clear')
-
-			#Print the logs
-			print (asa_logs)
-
-			#Wait before repeating
-			time.sleep(5)
-	except KeyboardInterrupt():
-		pass
+	asa_show_command = "show logging asdm"
 else:
-	#Collect logs until Ctrl+D
-	try:
-		while True:
-			#Get current logs and append them to the existing variable
-			asa_logs += asa_ssh_connection.send_command('show logging asdm' + asa_host_focus)
+	asa_show_command = "show logging asdm | inc " + asa_host_focus
 
-			#Clear the screen
-			os.system('cls' if os.name == 'nt' else 'clear')
+# Collect logs until Ctrl+C
+try:
+	while True:
+		#Get current logs and append them to the existing variable
+		asa_logs += asa_ssh_connection.send_command(asa_show_command)
 
-			#Print the logs
-			print (asa_logs)
+		#Clear the screen
+		os.system('cls' if os.name == 'nt' else 'clear')
 
-			#Wait before repeating
-			time.sleep(5)
-	except KeyboardInterrupt():
-		pass
+		#Print the logs
+		print (asa_logs)
+		print ("#" * 70)
+		print ("[*] Press Ctrl+C to stop")
 
-#Disconnect SSH session
+		#Wait before repeating
+		time.sleep(5)
+except KeyboardInterrupt:
+	pass
+
+# Disconnect SSH session
 asa_ssh_connection.disconnect()
+
+# Ask if user would like to save log output to a file
+save_file = input("\nWould you like to save the logs to a file [y/n]?: ")
+
+# Check if the user choose to save the logs
+if save_file == "y" or save_file == "Y" or save_file == "yes" or save_file == "Yes":
+	print ("[*] Saving file asa_logs.txt")
+
+	# Build log file name
+	asa_log_file_name = "asa_logs.host.txt"
+
+	# Open the file for writing
+	asa_log_file = open ("asa_logs.txt","w")
+
+	# Write variable contents to file
+	asa_log_file.write(asa_logs)
+
+	# Close file
+	asa_log_file.close()
+else:
+	sys.exit("[*] Exiting")
